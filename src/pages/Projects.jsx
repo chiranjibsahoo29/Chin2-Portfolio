aae74a4a562597b5cf5afd80ce5ed9d4d1bdba41
Chiranjib Sahoo <chiranjibsahoo966@gmail.com>
2026-03-09 23:03:27 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -33,7 +33,7 @@ const PROJECTS = [
   ss: '/portfolio.jpg',
   tech: ['React', 'Framer Motion', 'Tailwind CSS'],
   live: '#',
-  code: 'https://github.com/kunj2803/Kunj-Portfolio'
+  code: 'https://github.com/chiranjibsahoo29/Chin2-Portfolio.git'
   },
   {
     title: 'AgriVision 🌾🚜',
